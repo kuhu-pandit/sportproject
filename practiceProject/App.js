7f35aacd5170c9cfc80d590884e68cea233ef69d
Kuhu Pandit <kuhup2003@gmail.com>
2023-06-26 00:07:03 -0500

    fixed screen nav

--- practiceProject/App.js
+++ practiceProject/App.js
@@ -1,71 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import SignIn from './screens/SignIn';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SignUp from "./screens/SignUp";
+
+
+const Stack = createNativeStackNavigator();
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-
-      <View style={styles.welcomeContainer}>
-        <Text style={styles.title}>Welcome!</Text>
-
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.loginbutton}>Sign In</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.button1}>
-          <Text style={styles.loginbutton1}>Sign Up</Text>
-        </TouchableOpacity>
-
-
-      </View>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        { <Stack.Screen name="SignIn" component={SignIn} />}
+        { <Stack.Screen name="SignUp" component={SignUp} />}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#189AB4",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-
-  welcomeContainer: {
-    top: -250,
-  },
-
-  title: {
-    fontSize: 50,
-    color: "white",
-  },
-  button: {
-    fontSize: 35,
-    color: "white",
-    top: 430,
-    alignItems: "center",
-  },
-  loginbutton: {
-    fontSize: 29,
-    color: "white",
-    borderWidth: 1,
-    borderColor: '#ffafcc',
-    borderRadius: 10,
-    paddingVertical: 5, 
-    paddingHorizontal: 60,
-  },
-  button1: {
-    fontSize: 32,
-    color: "white",
-    top: 450,
-    alignItems: "center",
-  },
-  loginbutton1: {
-    fontSize: 27,
-    color: "white",
-    borderWidth: 1,
-    borderColor: '#ffafcc',
-    borderRadius: 10,
-    paddingVertical: 5, 
-    paddingHorizontal: 55,
-  },
-});
---
+++ practiceProject/screens/SignUp.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+const SignUp = ()=>{
+    return(
+        
+  <Text> Hello World</Text>
+       
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#189AB4",
+      alignItems: 'flex-start',
+      justifyContent: "center",
+    },
+  
+    welcomeContainer: {
+      top: -250,
+    },
+
+  });
+
+export default SignIn
